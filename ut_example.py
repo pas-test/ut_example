7216185eb2f10337e38b15746b4cc5dbd6857982
Pavel Svatos <pavel.svatos666@gmail.com>
2020-04-06 17:36:56 +0200

    Description:
    change default IP
    
    Signed-off-by: Pavel Svatos <pavel.svatos666@gmail.com>

--- ut_example.py
+++ ut_example.py
@@ -3,20 +3,17 @@
 import unittest
 import subprocess
 
-__author__ = 'Pavel Svatos'
-
-
-IP_FRANKENSTEIN_PLC = '192.168.1.150'
+IP_FRANKENSTEIN_PLC = '192.168.1.19'
 SSH_PORT = '22'
 
 
 class Testfunc(unittest.TestCase):
-    
+
     def call_cmd(self, cmd):
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
         stdout, __ = p.communicate()
         return stdout.decode('utf-8')
-            
+
     def test_ping_FRANKENSTEIN_PLC(self):
         cmd = f'ping {IP_FRANKENSTEIN_PLC} -c 1 -W 1'
         out = self.call_cmd(cmd)
@@ -27,7 +24,7 @@ class Testfunc(unittest.TestCase):
         cmd = f'nc -zv {IP_FRANKENSTEIN_PLC} {SSH_PORT} -w 1'
         out = self.call_cmd(cmd)
         self.assertTrue('[tcp/ssh] succeeded' in out, 'SSH connection refused!')
-        print('\nTest output: SSH connection allowed!')        
-        
+        print('\nTest output: SSH connection allowed!')
+
 if __name__ == '__main__':
     unittest.main()
